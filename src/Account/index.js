0353f888088e563334c2058859a7fddfdb9203b3
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-10-29 19:47:35 -0600

    everything works, idk if i did the last one correctly

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      balance:0,
+    }
+  }
+
+  handleDepositClick=(e)=>{
+    e.preventDefault();
+    const amount = parseInt(this.inputBox.value);
+    const newBalance = this.state.balance + amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.inputBox.value = '';
+  }
+  handleWithdrawClick=(e)=>{
+    e.preventDefault();
+    const amount = parseInt(this.inputBox.value);
+    const newBalance = this.state.balance - amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.inputBox.value = '';
+  }
   render() {
+    let zeroClass = 'balance';
+      if(this.state.balance === 0){
+        zeroClass += ' zero';
+      }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={zeroClass}>{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
+        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
+        
       </div>
     )
   }
